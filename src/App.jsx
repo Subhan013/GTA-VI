5a6b3924d14ea08e021b1f5ebcc8835b4c091ae5
SubhanDhedhi <151031703+Subhan013@users.noreply.github.com>
2025-08-13 16:43:38 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -94,7 +94,127 @@ function App() {
 
   return (
     <>
-     <h1>hello<h1/>
+      <div className="svg flex items-center justify-center fixed top-0 left-0 z-[100] w-full h-screen overflow-hidden bg-[#000]">
+        <svg viewBox="0 0 800 600" preserveAspectRatio="xMidYMid slice">
+          <defs>
+            <mask id="viMask">
+              <rect width="100%" height="100%" fill="black" />
+              <g className="vi-mask-group">
+                <text
+                  x="50%"
+                  y="50%"
+                  fontSize="250"
+                  textAnchor="middle"
+                  fill="white"
+                  dominantBaseline="middle"
+                  fontFamily="Arial Black"
+                >
+                  VI
+                </text>
+              </g>
+            </mask>
+          </defs>
+          <image
+            href="./bg.png"
+            width="100%"
+            height="100%"
+            preserveAspectRatio="xMidYMid slice"
+            mask="url(#viMask)"
+          />
+        </svg>
+      </div>
+      {showContent && (
+        <div className="main w-full rotate-[-10deg] scale-[1.7]">
+          <div className="landing overflow-hidden relative w-full h-screen bg-black">
+            <div className="navbar absolute top-0 left-0 z-[10] w-full py-10 px-10">
+              <div className="logo flex gap-7">
+                <div className="lines flex flex-col gap-[5px]">
+                  <div className="line w-15 h-2 bg-white"></div>
+                  <div className="line w-8 h-2 bg-white"></div>
+                  <div className="line w-5 h-2 bg-white"></div>
+                </div>
+                <h3 className="text-4xl -mt-[8px] leading-none text-white">
+                  Rockstar
+                </h3>
+              </div>
+            </div>
+
+            <div className="imagesdiv relative overflow-hidden w-full h-screen">
+              <img
+                className="absolute sky scale-[1.5] rotate-[-20deg] top-0 left-0 w-full h-full object-cover"
+                src="./sky.png"
+                alt=""
+              />
+              <img
+                className="absolute scale-[1.8] rotate-[-3deg] bg top-0 left-0 w-full h-full object-cover"
+                src="./bg.png"
+                alt=""
+              />
+              <div className="text text-white flex flex-col gap-3 absolute top-20 left-1/2 -translate-x-1/2 scale-[1.4] rotate-[-10deg]">
+                <h1 className="text-[15vw] leading-none -ml-40">grand</h1>
+                <h1 className="text-[15vw] leading-none ml-20">theft</h1>
+                <h1 className="text-[15vw] leading-none -ml-40">auto</h1>
+              </div>
+              <img
+                className="absolute character -bottom-[31vw] left-1/2 -translate-x-1/2  scale-[9] rotate-[-20deg]"
+                src="./girlbg.png"
+                alt=""
+              />
+            </div>
+            <div className="btmbar text-white absolute bottom-0 left-0 w-full py-15 px-10 bg-gradient-to-t from-black to-transparent">
+              <div className="flex gap-4 items-center">
+                <i className="text-4xl ri-arrow-down-line"></i>
+                <h3 className="text-xl font-[Helvetica_Now_Display]">
+                  Scroll Down
+                </h3>
+              </div>
+              <img
+                className="absolute h-[55px] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+                src="./ps5.png"
+                alt=""
+              />
+            </div>
+          </div>
+          <div className="w-full h-screen flex items-center justify-center bg-black">
+            <div className="cntnr flex text-white w-full h-[80%] ">
+              <div className="limg relative w-1/2 h-full">
+                <img
+                  className="absolute scale-[0.7] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+                  src="./imag.png"
+                  alt=""
+                />
+              </div>
+              <div className="rg w-[30%] py-0">
+                <h1 className="text-4xl">Still Running,</h1>
+                <h1 className="text-4xl">Not Hunting</h1>
+                <p className="mt-10 text-[14px] font-[Helvetica_Now_Display]">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Distinctio possimus, asperiores nam, omnis inventore nesciunt
+                  a architecto eveniet saepe, ducimus necessitatibus at
+                  voluptate.
+                </p>
+                <p className="mt-3 text-[14px] font-[Helvetica_Now_Display]">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. At
+                  eius illum fugit eligendi nesciunt quia similique velit
+                  excepturi soluta tenetur illo repellat consectetur laborum
+                  eveniet eaque, dicta, hic quisquam? Ex cupiditate ipsa nostrum
+                  autem sapiente.
+                </p>
+                <p className="mt-10 text-[14px] font-[Helvetica_Now_Display]">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. At
+                  eius illum fugit eligendi nesciunt quia similique velit
+                  excepturi soluta tenetur illo repellat consectetur laborum
+                  eveniet eaque, dicta, hic quisquam? Ex cupiditate ipsa nostrum
+                  autem sapiente.
+                </p>
+                <button className="bg-yellow-500 px-7 py-7 text-black mt-10 text-3xl">
+                  Download Now
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 }
